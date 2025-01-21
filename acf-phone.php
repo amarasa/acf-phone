f2347ab36fb33168e0f624cf1bde7684b68c87a5
amarasa <lpangm03@yahoo.com>
2025-01-21 12:19:48 -0500

    Update acf-phone.php
    Fixed: Deprecated: Creation of dynamic property acf_plugin_phone::$settings is deprecated in /Users/angelom/Sites/mpm-wp/wp-content/plugins/acf-phone-master/acf-phone.php on line 33

--- acf-phone.php
+++ acf-phone.php
@@ -12,23 +12,30 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
 // exit if accessed directly
 if( ! defined( 'ABSPATH' ) ) exit;
+
 // check if class already exists
 if( !class_exists('acf_plugin_phone') ) :
+
 class acf_plugin_phone {
 
+	/**
+	 * Settings property to hold plugin configuration.
+	 *
+	 * @var array
+	 */
+	private $settings;
+
 	/*
-	*  __construct
-	*
-	*  This function will setup the class functionality
-	*
-	*  @since	1.0.0
-	*
-	*  @param	n/a
-	*  @return	n/a
-	*/
-
+	 * __construct
+	 *
+	 * This function will setup the class functionality
+	 *
+	 * @since	1.0.0
+	 *
+	 * @param	n/a
+	 * @return	n/a
+	 */
 	function __construct() {
-
 		// vars
 		$this->settings = array(
 			'version'	=> '1.0.0',
@@ -36,45 +43,36 @@ class acf_plugin_phone {
 			'path'		=> plugin_dir_path( __FILE__ )
 		);
 
-
 		// set text domain
 		// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
 		// load_plugin_textdomain( 'acf-phone', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' );
 
-
 		// include field
 		add_action('acf/include_field_types', 	array($this, 'include_field_types')); // v5
 		add_action('acf/register_fields', 		array($this, 'include_field_types')); // v4
-
 	}
 
-
 	/*
-	*  include_field_types
-	*
-	*  This function will include the field type class
-	*
-	*  @since	1.0.0
-	*
-	*  @param	$version (int) major ACF version. Defaults to false
-	*  @return	n/a
-	*/
-
+	 * include_field_types
+	 *
+	 * This function will include the field type class
+	 *
+	 * @since	1.0.0
+	 *
+	 * @param	$version (int) major ACF version. Defaults to false
+	 * @return	n/a
+	 */
 	function include_field_types( $version = false ) {
-
 		// support empty $version
 		if( !$version ) $version = 5;
 
-
 		// include
 		include_once('fields/acf-phone-v' . $version . '.php');
-
 	}
-
 }
+
 // initialize
 new acf_plugin_phone();
-// class_exists check
-endif;
 
+endif;
 ?>
